d8cb56b013c045fa6e8940fb26e0f28cbd61dcc3
sudhan-1999 <jsudhan68@gmail.com>
2024-03-13 11:46:13 +0530

    css is improved for detailed page

--- contextapi/src/Detailed.jsx
+++ contextapi/src/Detailed.jsx
@@ -1,65 +1,54 @@
 import React from "react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
-function Details({e}) {
-  const [count,setCount]=useState("1");
-  function plus(){
+function Details({ data }) {
+  const [count, setCount] = useState(1);
+  function plus() {
     setCount(count + 1);
   }
-  function minus(){
+  function minus() {
     setCount(count - 1);
   }
- //const {id} =useParams()
+  console.log(data);
+  const { id } = useParams();
   return (
-    <div>
+    <div className="detailedpage">
       <div className="row">
         <div className="col-lg-6">
-          <span><img src={e.images[0]} alt="mobiles.." /></span>
-          <span>{e.title}
-          <p>{e.description}</p></span>
+          <span className="span1">
+            <img src={data.title} alt="mobiles.." />
+          </span>
+        </div>
+        <div className="col-lg-6">
+          <h3 className="span2">
+            {data.title}-{id}title
+          </h3>
+          <p>{data.description}description</p>
         </div>
       </div>
-      <div className="col-lg-6">
-      <span>
-        <button onClick={plus}>+</button></span>
-        <span> <p>{count}</p></span>
-        <span><button onClick={minus}>-</button>
-        </span>
-        <span>${count*e.price}</span>
-      </div>
+
       <div className="row">
-      <div className="col-lg-6">
-        <div>
-          <p>Subtotal:</p>
-          <p>Shipping:</p>
+        <div className="col-lg-6">
+          <div className="span3">
+            <button onClick={minus}>-</button>
+            <span className="span4">{count}</span>
+            <button onClick={plus}>+</button>
+          </div>
+          <div className="span5">${count * data.price}</div>
+        </div>
+        <div className="col-lg-6">
+          <div>
+            <p>Subtotal:${count * data.price}</p>
+            <p>Shipping:Free</p>
+          </div>
         </div>
       </div>
-      <div className="col-lg-6">
-        <div>
-          <p>${count*e.price}</p>
-          <p>Free</p>
-        </div>
+      <div>
+        <h4>Total:${count * data.price}</h4>
       </div>
-      </div>
-      <div><h4>Total:${count*e.price}</h4></div>
     </div>
   );
 }
-/*id: 5,
-      title: "Huawei P30",
-      description:
-        "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
-      price: 499,
-      discountPercentage: 10.58,
-      rating: 4.09,
-      stock: 32,
-      brand: "Huawei",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
-      images: [
-        "https://i.dummyjson.com/data/products/5/1.jpg",
-        "https://i.dummyjson.com/data/products/5/2.jpg",
-        "https://i.dummyjson.com/data/products/5/3.jpg",
-      ],*/
 
 export default Details;
--- contextapi/src/products.jsx
+++ contextapi/src/products.jsx
@@ -1,107 +1,17 @@
 import React from "react";
-import Details from "./Detailed" 
+//import Details from "./Detailed" 
 import { useNavigate } from "react-router-dom";
 
-function Productslist() {
+function Productslist({data}) {
  const navigate = useNavigate();
-  const data = [
-    {
-      id: 1,
-      title: "iPhone 9",
-      description: "An apple mobile which is nothing like apple",
-      price: 549,
-      discountPercentage: 12.96,
-      rating: 4.69,
-      stock: 94,
-      brand: "Apple",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-      images: [
-        "https://i.dummyjson.com/data/products/1/1.jpg",
-        "https://i.dummyjson.com/data/products/1/2.jpg",
-        "https://i.dummyjson.com/data/products/1/3.jpg",
-        "https://i.dummyjson.com/data/products/1/4.jpg",
-        "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-      ],
-    },
-    {
-      id: 2,
-      title: "iPhone X",
-      description:
-        "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-      price: 899,
-      discountPercentage: 17.94,
-      rating: 4.44,
-      stock: 34,
-      brand: "Apple",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-      images: [
-        "https://i.dummyjson.com/data/products/2/1.jpg",
-        "https://i.dummyjson.com/data/products/2/2.jpg",
-        "https://i.dummyjson.com/data/products/2/3.jpg",
-        "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-      ],
-    },
-    {
-      id: 3,
-      title: "Samsung Universe 9",
-      description:
-        "Samsung's new variant which goes beyond Galaxy to the Universe",
-      price: 1249,
-      discountPercentage: 15.46,
-      rating: 4.09,
-      stock: 36,
-      brand: "Samsung",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
-      images: ["https://i.dummyjson.com/data/products/3/1.jpg"],
-    },
-    {
-      id: 4,
-      title: "OPPOF19",
-      description: "OPPO F19 is officially announced on April 2021.",
-      price: 280,
-      discountPercentage: 17.91,
-      rating: 4.3,
-      stock: 123,
-      brand: "OPPO",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
-      images: [
-        "https://i.dummyjson.com/data/products/4/1.jpg",
-        "https://i.dummyjson.com/data/products/4/2.jpg",
-        "https://i.dummyjson.com/data/products/4/3.jpg",
-        "https://i.dummyjson.com/data/products/4/4.jpg",
-        "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
-      ],
-    },
-    {
-      id: 5,
-      title: "Huawei P30",
-      description:
-        "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
-      price: 499,
-      discountPercentage: 10.58,
-      rating: 4.09,
-      stock: 32,
-      brand: "Huawei",
-      category: "smartphones",
-      thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
-      images: [
-        "https://i.dummyjson.com/data/products/5/1.jpg",
-        "https://i.dummyjson.com/data/products/5/2.jpg",
-        "https://i.dummyjson.com/data/products/5/3.jpg",
-      ],
-    },
-  ];
+  
  
   return (
     <div className="container">
       <div className="row">
-        {data.map((e, id) => {
+        {data.map((e, index) => {
           return (
-            <div className="col-lg-4" key={id}>
+            <div className="col-lg-4" key={index}>
               <div className="card" style={{ width: "18rem", height: "22rem" }}>
                 <img
                   style={{ width: "10rem", height: "15rem" }}
@@ -122,12 +32,12 @@ function Productslist() {
                   <p>DiscountPercentage:{e.discountPercentage}%</p>
                   <p>Stocks:{e.stock}</p>
                   <button
-                    onClick={() => navigate(`/Productslist/${e.id}`)}
+                    onClick={() => navigate("/Productslist/"+e.id)}
                     className="btn btn-primary"
                   >
                     Buy Now
                   </button>
-                  <Details e={e} id={id}/>
+                  
                 </div>
               </div>
             </div>
