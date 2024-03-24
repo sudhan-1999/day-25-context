234bf54acfc00075175b49ee4aa2df06282c40a9
sudhan-1999 <jsudhan68@gmail.com>
2024-03-24 18:16:36 +0530

    context

--- contextapi/src/Detailed.jsx
+++ contextapi/src/Detailed.jsx
@@ -1,17 +1,29 @@
-import React, { useContext } from "react";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-//import {ProductContext} from './productcontextprovider';
 
-function Details({ data }) {
-  //const{count,plus,minus}=useContext(ProductContext)
+import React, { useContext, useState, createContext } from "react";
+import { useParams } from "react-router-dom";
+
+export const ProductContext = createContext();
+
+export const ProductContextProvider = ({ children }) => {
   const [count, setCount] = useState(1);
+
   function plus() {
     setCount(count + 1);
   }
+
   function minus() {
     setCount(count - 1);
   }
+
+  return (
+    <ProductContext.Provider value={{ count, plus, minus }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+function Details({ data }) {
+  const { count, plus, minus } = useContext(ProductContext) ;
   const { id } = useParams();
   const product = data[id];
 
@@ -19,10 +31,10 @@ function Details({ data }) {
     return (
       <div
         style={{
-          "margin-left": "400px",
-          "margin-top": "200px",
+          marginLeft: "400px",
+          marginTop: "200px",
           fontSize: "50px",
-          "font-weight": "bold",
+          fontWeight: "bold",
         }}
       >
         Product not found!.....
@@ -31,7 +43,6 @@ function Details({ data }) {
   }
 
   return (
-    
     <div className="detailedpage">
       <div className="row">
         <div className="col-lg-6">
@@ -48,7 +59,7 @@ function Details({ data }) {
       <div className="row">
         <div className="col-lg-6">
           <div className="span3">
-            <button className="bt"onClick={minus}>-</button>
+            <button className="bt" onClick={minus}>-</button>
             <span className="span4">{count}</span>
             <button className="bt" onClick={plus}>+</button>
           </div>
@@ -65,7 +76,16 @@ function Details({ data }) {
         <h4>Total:${count * product.price}</h4>
       </div>
     </div>
-    );
+  );
 }
 
-export default Details;
+export default function DetailsWrapper(props) {
+  // Assuming you pass 'data' as a prop to DetailsWrapper
+  return (
+    <ProductContextProvider>
+      <Details {...props} />
+    </ProductContextProvider>
+  );
+}
+
+
